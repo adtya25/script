13afdc3e8455a10052edde830f2c957882c08e26
adtya25 <79787334+adtya25@users.noreply.github.com>
2021-02-28 16:47:30 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,16 @@
+<?php
+
+# Mr.Black86 project
+# Thanks for subscribe CRYPTO GRATISAN
+# Doge donation : DTHY59Tmbhxq3gKwgPzdLSkjjbkCuUjifZ
+
+// Input login data
+$email = "xxxx";
+$pass = "xxxx";
+
+// Input cookie data
+$useragent = "xxxx";
+$tawkuuid = "xxxx";
+$cfduid = "xxxx";
+$atuvc = "xxxx";
+$atuvs = "xxxx";

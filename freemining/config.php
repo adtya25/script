6d977335888d844e2c725be06393aea6b6b6ab58
adtya25 <79787334+adtya25@users.noreply.github.com>
2021-03-01 02:17:53 +0700

    Add files via upload

---
+++ freemining/config.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+TIDAK ADA
+EXS EXG PIVX
+*/
+
+//DATA AKUN
+$user_agent = "xxx";
+
+$PHPSESSID = "xxx";
+
+//DATA TOKEN
+
+$doge = "xxx";
+
+$bcn = "xxx";
+
+$trx = "xxx";
+
+$dgb = "xxx";
+
+$ltc = "xxx";
+
+$dash = "xxx";
+
+$neo = "xxx";
+
+$ppc = "xxx";
+
+$waves = "xxx";
+
+$zec = "xxx";
+
+$lsk = "xxx";
+
+$eth = "xxx";
+
+$strax = "xxx";
+
+$rdd = "xxx";
+
+$bch = "xxx";
+
+$ada = "xxx";
+
+$etc = "xxx";
+
+$zen = "xxx";
+
+$kmd = "xxx";
+
+$xtz = "xxx";
+
+$vtc = "xxx";
+
+$xmr = "xxx";
+
+
+?>
